a9d97db6d9637822dfd8300fd35ae71d14a74530
Edna Gomez Pallares <ednagomezpallares@Ednas-MBP.lan>
2024-03-11 23:56:35 -0400

    created tables

--- server.js
+++ server.js
@@ -1,25 +1,24 @@
 const pg = require("pg");
 const client = new pg.Client(
-  process.env.DATABASE_URL || "postgres://localhost/acme_notes_catrgories_db"
+  process.env.DATABASE_URL || "postgres://localhost/acme_deparments_employees_db"
 );
 
 const init = async () => {
   await client.connect();
+  console.log('connected to database')
   let SQL = `
-      DROP TABLE IF EXISTS departments;
       DROP TABLE IF EXISTS employees;
+      DROP TABLE IF EXISTS departments;
       CREATE TABLE departments(
         id SERIAL PRIMARY KEY,
-        name VARCHAR(100)
+        name VARCHAR(100) NOT NULL
       );
       CREATE TABLE employees(
         id SERIAL PRIMARY KEY,
-        name VARCHAR(100),
+        name VARCHAR(255) NOT NULL,
         created_at TIMESTAMP DEFAULT now(),
         updated_at TIMESTAMP DEFAULT now(),
-        department_id INTEGER DEFAULT 3 NOT NULL,
-        txt VARCHAR(255) NOT NULL,
-        department_id INTEGER REFERENCES categories(id) NOT NULL
+        department_id INTEGER REFERENCES departments(id) NOT NULL
       );
     `;
   await client.query(SQL);
